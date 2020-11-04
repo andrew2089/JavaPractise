f5da9fba296bb73ff0de10b16b4d3a03da1fd145
andrew2089 <aglushenok9@gmail.com>
2020-11-04 16:35:58 +0200

    MathOperations

---
+++ src/l4/booleanDemo.java
@@ -0,0 +1,22 @@
+package l4;
+
+public class booleanDemo {
+    public static void main(String[] args) {
+        int x = 50;
+
+        System.out.println("before");
+        if((x>= 1)&&(x<= 100) && (x % 2 == 0) ){
+            System.out.println("x in bounds and even");
+        }
+        System.out.println("after");
+
+        int y = 75;
+        System.out.println("second part");
+        System.out.println("before");
+        if((y>= 1)&&(y<= 100) || (y % 2 == 0) ){
+            System.out.println("y in bounds or even");
+        }
+        System.out.println("after");
+    }
+
+}
